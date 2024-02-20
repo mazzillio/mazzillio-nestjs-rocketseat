5aa28e262b1eb642d8bb98b89811f1ad820ae1ea
mazzillio <mateca500@gmail.com>
2024-02-20 19:07:12 -0300

    add env config service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { TypeEnv } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const configService: ConfigService<TypeEnv, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
