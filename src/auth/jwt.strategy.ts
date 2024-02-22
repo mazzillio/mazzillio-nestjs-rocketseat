d1f1e6146e666940cc34cf345cb847a1c2bc4a5b
mazzillio <mateca500@gmail.com>
2024-02-22 09:13:35 -0300

    create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { TypeEnv } from 'src/env'
+import { z } from 'zod'
+
+const tokenSchema = z.object({
+  sub: z.string().uuid(),
+})
+type TokenSchema = z.infer<typeof tokenSchema>
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private config: ConfigService<TypeEnv, true>) {
+    const publickey = config.get('JWT_PUBLIC', { infer: true })
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Buffer.from(publickey, 'base64'),
+      algorithms: ['RS256'],
+    })
+  }
+
+  async validate(payload: TokenSchema) {
+    return tokenSchema.parse(payload)
+  }
+}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('questions')
+@UseGuards(AuthGuard('jwt'))
+export class CreateQuestionController {
+  constructor(private prismaService: PrismaService) {}
+  @Post()
+  async handle() {
+    return 'ok'
+  }
+}
