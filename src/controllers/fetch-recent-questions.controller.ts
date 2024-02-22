ff49235b7c44e832986c47d494027819c14366b9
mazzillio <mateca500@gmail.com>
2024-02-22 10:28:30 -0300

    create controller list questions

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+@Controller('/questions')
+export class FetchRecentQuestionsController {
+  constructor(private prismaService: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+    const questions = await this.prismaService.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    return { questions }
+  }
+}
